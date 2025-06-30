6a7b313b2e8be0c3d8e44ebaee49229d69966144
Isfandiyor Mullajonov <114288417+iscaDev@users.noreply.github.com>
2025-06-30 15:20:12 +0500

    Update comparison.py

--- comparison.py
+++ comparison.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import numpy
 import random
 
+#My implementation of Fisher-Yates algorithm
 def fisher_yates(array):
     n=len(array)
     for i in range(n-1, 0, -1):  
@@ -14,13 +15,13 @@ def fisher_yates(array):
 
     return array 
 
-
+#NumPy permutation algorithm
 def numpy_implementation(arr):
     return numpy.random.permutation(arr)
 
 
 
-sizes = [100, 1000, 5000, 10000, 20000, 30000, 40000, 50000, 60000, 70000, 80000, 90000, 100000]
+sizes = [100, 1000, 5000, 10000, 20000, 30000, 40000, 50000, 60000, 70000, 80000, 90000, 100000] # different sizes to test the algorithms on
 fisher_times = []
 numpy_times = []
 
@@ -38,6 +39,7 @@ for size in sizes:
     numpy_implementation(arr_np)
     numpy_times.append((time.time() - start)*1000)
 
+#Here to plot the comparison of their execution time
 plt.plot(sizes, fisher_times, label='Fisher-Yates', marker='o')
 plt.plot(sizes, numpy_times, label='NumPy Implementation', marker='o')
 plt.xlabel('Input Size (n)')
